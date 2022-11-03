8e1bb119f8e4687766d6b33dcc8bf17ae33040a4
kakubin <wetsand.wfs@gmail.com>
2022-11-03 22:01:16 +0900

    fix configuration architecture

--- lib/action_slack/configuration.rb
+++ lib/action_slack/configuration.rb
@@ -2,23 +2,30 @@
 
 module ActionSlack
   class Configuration
-    attr_accessor :filepath
+    attr_accessor :filepath, :async
+
+    def self.set_default(name, default)
+      define_method name do
+        @configuration[name]
+      end
+
+      define_method :"#{name}=" do |value|
+        @configuration[name] = value
+      end
+
+      default_configuration[name] = default
+    end
+
+    def self.default_configuration
+      @default_configuration ||= {}
+    end
 
     def initialize
-      @filepath = 'config/slack_webhooks.yml'
-      async!
+      @configuration = self.class.default_configuration.dup
     end
 
-    def async!
-      @async = true
-    end
-
-    def sync!
-      @async = false
-    end
-
-    def async?
-      @async
-    end
+    set_default(:filepath, 'config/slack_webhooks.yml')
+    set_default(:async, true)
+    set_default(:env, :development)
   end
 end
--- spec/action_slack/configuration_spec.rb
+++ spec/action_slack/configuration_spec.rb
@@ -1,3 +1,13 @@
 # frozen_string_literal: true
 
-RSpec.describe ActionSlack::Configuration
+RSpec.describe ActionSlack::Configuration do
+  describe '.set_default' do
+    subject { described_class.set_default(:key, :new_value) }
+
+    it 'will be changed' do
+      expect(described_class.default_configuration[:key]).to be_nil
+      subject
+      expect(described_class.default_configuration[:key]).to be :new_value
+    end
+  end
+end
